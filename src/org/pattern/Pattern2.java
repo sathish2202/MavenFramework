dda21255f189b0a3f257b0c19e4043c3912df38c
sathish2202 <sathishmuthukalai@gmail.com>
2021-10-26 15:37:43 +0530

    i have added some code

--- src/org/pattern/Pattern2.java
+++ src/org/pattern/Pattern2.java
@@ -1,6 +1,10 @@
 package org.pattern;
 
 public class Pattern2 {
+	public void empId() {
+		System.out.println("2217");
+	}
+
 	public static void main(String[] args) {
 		int rows = 5;
 		for (int i = 1; i <= 5; i++) {
